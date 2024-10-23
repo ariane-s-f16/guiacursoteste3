b530396029e131d0533d4f1f965bad2e5b574476
Usuario <Usuario@CPD03-M11>
2024-10-23 14:48:52 -0300

    Adicionar arquivos de projeto.

---
+++ guiacursoteste3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace guiacursoteste3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ guiacursoteste3/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace guiacursoteste3
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
